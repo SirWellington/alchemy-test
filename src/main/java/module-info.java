ccd710e6fc63325513fa52cb51d792613e611190
Wellington Moreno <JWellington.Moreno@Gmail.com>
2026-06-19 13:18:53 -0700

    Create module-info.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module tech.sirwellington.alchemy.test {
+    requires alchemy.generator;
+    requires java.sql;
+    requires junit;
+    requires org.mockito;
+    requires org.hamcrest;
+
+    exports tech.sirwellington.alchemy.test;
+}
