3add677e744db25626cd89f4a8b6b957a3ea0f1a
Wellington Moreno <JWellington.Moreno@Gmail.com>
2026-06-23 17:14:43 -0700

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,13 +2,10 @@ module tech.sirwellington.alchemy.test {
     requires java.sql;
     requires org.junit.jupiter.api;
     requires org.junit.jupiter.engine;
-    requires org.mockito;
     requires org.slf4j;
     requires tech.sirwellington.alchemy.annotations;
     requires tech.sirwellington.alchemy.generator;
-    requires org.mockito.junit.jupiter;
 
     exports tech.sirwellington.alchemy.test.junit;
     exports tech.sirwellington.alchemy.test.junit.generation;
-    exports tech.sirwellington.alchemy.test.mockito;
 }
