18eb34be512897cb5590f74876c1d74cb34710a1
Wellington Moreno <JWellington.Moreno@Gmail.com>
2026-07-06 15:17:10 -0700

    Module stuff

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,13 @@
 module tech.sirwellington.alchemy.test {
-    exports  tech.sirwellington.alchemy.test;
-    exports  tech.sirwellington.alchemy.test.generation;
+    requires java.base;
+    requires java.sql;
+    requires org.junit.jupiter;
+    requires org.slf4j;
+    requires tech.sirwellington.alchemy.annotations;
+    requires tech.sirwellington.alchemy.generator;
+
+    exports tech.sirwellington.alchemy.test;
+    exports tech.sirwellington.alchemy.test.generation;
+
+    opens tech.sirwellington.alchemy.test to org.junit.platform.commons;
 }
